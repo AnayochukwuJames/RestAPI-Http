76c1b2c48888e20f45739157bbe34339581dbc13
AnayochukwuJames <anayochukwujames11@gmail.com>
2023-12-02 22:54:39 +0100

    updating initial commit

--- src/main/java/com/example/restapihttp/dto/request/SignUpRequest.java
+++ src/main/java/com/example/restapihttp/dto/request/SignUpRequest.java
@@ -1,5 +1,7 @@
 package com.example.restapihttp.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -8,7 +10,11 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class SignUpRequest {
+    @NotBlank(message = "first name must not be blank")
+    @Size(min = 3, message = "first name must be minimum of 3 character")
     private String firstName;
+    @NotBlank(message = "last name must not be blank")
+    @Size(min = 3, message = "last name must be minimum of 3 character")
     private String lastName;
     private String email;
     private String phoneNumber;
